8db1f6f7680d2d7eba7e14c261753554f181963d
Gin <forevermetin2@gmx.net>
2021-08-01 17:36:31 +0200

    1.0
    ADD root / etc / sound / index

---
+++ pack/root/color.py
@@ -0,0 +1,11 @@
+desc_done = #8D9440
+desc_system = #4F84C4
+desc_fail = #DD4132
+desc_info = #D5AE41
+desc_normal = #D1B894
+desc_tooltip = #EAE6DA
+disc_error = #BFD641
+
+
+
+progresbar = #BFD641
