dde47b8134ee40742dd53d8c0d7d7d790c5b38f6
Gin <forevermetin2@gmx.net>
2021-08-21 01:15:54 +0200

    v.2.0
    ADD bgm,locale

---
+++ pack/locale/fem/locale_string.lua
@@ -0,0 +1,15 @@
+-- Localestring of Forevermetin2 --
+-- You can translate these files into your language as you wish.
+
+function question ace
+	ask_ace0 = "How should I start mapping?"
+	ask_ace1 = "What should I consider when mapping?"
+	ask_ace2 = "Which map sizes are ideal"
+	ask_ace3 = "Is there a magic trick to make the maps look nice?"
+	ask_ace4 = "Is it worth mapping? Can I earn anything with it?"
+	ask_ace5 = "Where can i find customers who buy my maps?"
+	ask_ace6 = "Is it legal to sell maps?"
+	ask_ace7 = "Can I use other people's objects and then sell the map?"
+end
+
+function 
