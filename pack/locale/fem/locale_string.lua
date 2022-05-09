d938a2dfcc108b36134399115d3e7d06521225fb
Forevermetin2 <forevermetin2@switzerland.net>
2022-05-10 01:24:00 +0200

    Update
    Changes in ETC/LOCALE

--- pack/locale/fem/locale_string.lua
+++ pack/locale/fem/locale_string.lua
@@ -1,15 +1,25 @@
--- Localestring of Forevermetin2 --
--- You can translate these files into your language as you wish.
+-- Localestring of Forevermetin2 v2.0 --
+-- Autor: https://metin2.dev/profile/40-ace/
+-- Website: https://www.forevermetin2.ovh
+-- Caution: Any change that does not correspond to the original must not be passed off as original unless it is translated and adopted 1 to 1.
 
-function question ace
-	ask_ace0 = "How should I start mapping?"
-	ask_ace1 = "What should I consider when mapping?"
-	ask_ace2 = "Which map sizes are ideal"
-	ask_ace3 = "Is there a magic trick to make the maps look nice?"
-	ask_ace4 = "Is it worth mapping? Can I earn anything with it?"
-	ask_ace5 = "Where can i find customers who buy my maps?"
-	ask_ace6 = "Is it legal to sell maps?"
-	ask_ace7 = "Can I use other people's objects and then sell the map?"
+
+
+function Forevermetin2 Moving Buttons
+	FOREVERMETIN_AboutButton_INFO = "How was Forevermetin2 created? Are you also curious how? and why it exists at all? Then read through everything and give me your feedback."
+	FOREVERMETIN_ChangelogButton_INFO = "Here you see what is new and old. It's hard to keep everything clearly arranged, so there will only be a short version of what has been changed."
+	FOREVERMETIN_ComandsButton_INFO = "There are various commands that are very practical."
+	FOREVERMETIN_FAQButton_INFO = "Here you could find answers to your questions regarding forever metin2."
+	FOREVERMETIN_DonateButton_INFO = "Would you like to donate something to me or would you like to invest in Forevermetin2? better and stronger hoster? or new features?"
+	FOREVERMETIN_FeedbackButton_INFO = "Tell me your opinion, or want a file you've been looking for for a long time."
+	FOREVERMETIN_OptionsButton_INFO = "Whenever you change the options, you should restart the tool."
+	FOREVERMETIN_SourceButton_INFO = "I don't know why it's here, but it's here. Something with source, but... do I want to release it or sell it?"
+	FOREVERMETIN_UpdateButton_INFO = "If the update function is used too much, only few will have access to new updates. Update only once a day, or check in Changelog."
+	FOREVERMETIN_TeamPatcherButton_INFO = "Collaborate with friends and keep them up to date."
+	FOREVERMETIN_PserverButton_INFO = "Here you find everything around Pserver and how to."
+	FOREVERMETIN_WorldEditorButton_INFO = "If you want to start with Mapping, be sure to check out this section, you will not regret it."
+	FOREVERMETIN_ProfinderButton_INFO = "As soon as this feature is finished, we save a lot of time with sorting out and providing maps."
+	FOREVERMETIN_PatcherStopButton_INFO = "With this you can stop the patcher immediately in case of errors."
+	FOREVERMETIN_ItemshopButton_INFO = "You need data.dll for that. If you do not have this, contact me."
+	FOREVERMETIN_RefreshButton_INFO = "With this you can update the interface if you have translated it for you."
 end
-
-function 
